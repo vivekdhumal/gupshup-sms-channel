3d6fcb18c6778c404f9c31cad176db27695b093e
Vivek Dhumal <vivekdhumalmail@gmail.com>
2024-12-08 21:44:56 +0530

    updated config file to use env variable

--- src/config/gupshup.php
+++ src/config/gupshup.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'userid' => '',
-    'password' => '',
-    'mask' => '',
-    'entity_id' => '',
-    'template_id' => '',
+    'userid' => env('GUPSHUP_USERID'),
+    'password' => env('GUPSHUP_PASSWORD'),
+    'mask' => env('GUPSHUP_MASK'),
+    'entity_id' => env('GUPSHUP_ENTITYID'),
+    'template_id' => env('GUPSHUP_TEMPLATE_ID'),
 ];
