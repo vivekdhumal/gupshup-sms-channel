4e0bce29f600f1ed88624383fb6ac5e9913430ec
Vivek Dhumal <vivekdhumalmail@gmail.com>
2024-12-08 20:22:54 +0530

    added gupshup response

--- src/GupshupChannel.php
+++ src/GupshupChannel.php
@@ -2,6 +2,7 @@
 
 namespace VivekDhumal\GupshupSmsChannel;
 
+use Exception;
 use Illuminate\Support\Facades\Http;
 
 class GupshupChannel
@@ -14,21 +15,40 @@ class GupshupChannel
             throw new \Exception('Recipient and message are required.');
         }
 
-        $response = Http::get('https://enterprise.smsgupshup.com/GatewayAPI/rest', [
-            'method' => 'SendMessage',
-            'send_to' => $message->to,
-            'msg' => $message->message,
-            'msg_type' => 'TEXT',
-            'userid' => $message->userid,
-            'auth_scheme' => 'plain',
-            'password' => $message->password,
-            'v' => '1.1',
-            'format' => 'JSON',
-            'mask' => $message->mask,
-            'principalEntityId' => $message->entityId,
-            'dltTemplateId' => $message->templateId,
-        ]);
+        try {
+            $response = Http::get('https://enterprise.smsgupshup.com/GatewayAPI/rest', [
+                'method' => 'SendMessage',
+                'send_to' => $message->to,
+                'msg' => $message->message,
+                'msg_type' => 'TEXT',
+                'userid' => $message->userid,
+                'auth_scheme' => 'plain',
+                'password' => $message->password,
+                'v' => '1.1',
+                'format' => 'JSON',
+                'mask' => $message->mask,
+                'principalEntityId' => $message->entityId,
+                'dltTemplateId' => $message->templateId,
+            ]);
 
-        return $response;
+            if($response->status() >= 200) {
+                $data = $response->json();
+
+                if($data && isset($data['response'])) {
+                    return new GupshupResponse($data['response']['status'], null, $data['response']['id'], $data['response']['details'], $data['response']['phone']);
+                } else {
+                    $arr = array_map('trim', explode('|', $response->body()));
+
+                    if(isset($arr[2])) {
+                        return new GupshupResponse('error', $arr[2]);
+                    }
+                }
+                return new GupshupResponse('error', 'No response from api');
+            } else {
+                return new GupshupResponse('error', 'SMS is not sent, status code : '.$response->status());
+            }
+        } catch(Exception $ex) {
+            return new GupshupResponse('error', 'SMS is not sent, error : '.$ex->getMessage());
+        }
     }
 }
